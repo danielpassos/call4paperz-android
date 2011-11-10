955ebd8aa683f664d90a223712009e350a2df65b
Daniel Passos <danpassos@gmail.com>
2011-11-10 08:42:28 -0200

    Add logo

--- gen/com/call4paperz/R.java
+++ gen/com/call4paperz/R.java
@@ -19,7 +19,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_menu_refresh=0x7f020000;
-        public static final int no_image=0x7f020001;
+        public static final int logo=0x7f020001;
+        public static final int no_image=0x7f020002;
     }
     public static final class event {
         public static final int comments=0x7f080009;
