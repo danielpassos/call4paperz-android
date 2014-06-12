d7571c80ce002aeb529e797c84a84dfa3caef319
Daniel Passos <daniel@passos.me>
2014-06-12 01:04:28 -0300

    Move Pipe from Application to Activity

--- src/com/call4paperz/android/activities/EventsActivity.java
+++ src/com/call4paperz/android/activities/EventsActivity.java
@@ -10,19 +10,28 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
-import com.call4paperz.android.Call4PaperzApplication;
 import com.call4paperz.android.R;
 import com.call4paperz.android.fragments.EventsFragments;
 import com.call4paperz.android.fragments.LoadFragment;
 import com.call4paperz.android.model.Event;
 import com.crashlytics.android.Crashlytics;
+import com.google.gson.GsonBuilder;
+
 import org.jboss.aerogear.android.Callback;
+import org.jboss.aerogear.android.Pipeline;
+import org.jboss.aerogear.android.impl.pipeline.PipeConfig;
 import org.jboss.aerogear.android.pipeline.LoaderPipe;
 
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
+import static com.google.gson.FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES;
+
 public class EventsActivity extends ActionBarActivity {
+
+    private LoaderPipe<Event> eventPipe;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,7 @@ public class EventsActivity extends ActionBarActivity {
         Crashlytics.start(this);
 
         setContentView(R.layout.main);
+        eventPipe = createPipe();
 
         loadEvents();
     }
@@ -47,12 +57,35 @@ public class EventsActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public LoaderPipe<Event> createPipe() {
+        try {
+
+            URL baseURL = new URL("http://call4paperz.com");
+
+            GsonBuilder gsonBuilder = new GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd")
+                    .setFieldNamingPolicy(LOWER_CASE_WITH_UNDERSCORES);
+
+            Pipeline pipeline = new Pipeline(baseURL);
+
+            PipeConfig pipeConfigEvent = new PipeConfig(baseURL, Event.class);
+            pipeConfigEvent.setName("events");
+            pipeConfigEvent.setEndpoint("events.json");
+            pipeConfigEvent.setGsonBuilder(gsonBuilder);
+            pipeline.pipe(Event.class, pipeConfigEvent);
+
+            return pipeline.get("events", this);
+
+        } catch (MalformedURLException e) {
+            Toast.makeText(getApplicationContext(), getString(R.string.an_error_occurred),
+                    Toast.LENGTH_SHORT).show();
+            return null;
+        }
+    }
+
     private void loadEvents() {
 
         displayFragment(new LoadFragment());
-
-        Call4PaperzApplication application = (Call4PaperzApplication) getApplication();
-        LoaderPipe<Event> eventPipe = application.getEventPipe(this);
 
         eventPipe.read(new Callback<List<Event>>() {
             @Override
